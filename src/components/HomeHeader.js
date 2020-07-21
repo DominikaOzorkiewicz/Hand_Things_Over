50b636e982c9bfa38c2680e6b219eb82a3d9ad1a
DominikaOzorkiewicz <62950674+DominikaOzorkiewicz@users.noreply.github.com>
2020-07-21 17:56:31 +0200

    Complete Hero component with navbar

--- src/components/HomeHeader.js
+++ src/components/HomeHeader.js
@@ -1,11 +1,11 @@
 import React from "react";
 import {Link} from "react-router-dom";
-import {Link as ScrollLink} from "react-scroll";
+import {Link as ScrollLink, animateScroll as scroll} from "react-scroll";
 
 export const HomeHeader = () => {
 
     return (
-        <header className='header'>
+        <header className='header' id='header'>
 
             <div className='header__user'>
                 <Link to='/login' className='header__user-link'>Zaloguj</Link>
@@ -15,19 +15,19 @@ export const HomeHeader = () => {
             <nav className="header__nav">
                 <ul className="nav__list">
                     <li className="nav__list-el">
-                        <ScrollLink activeClass="active" to="/" className="menu__list-link" spy={true} smooth={true}>Start</ScrollLink>
+                        <ScrollLink activeClass="active" to='header' className="menu__list-link" spy={true} smooth={true} duration={1000} onClick={() => scroll.scrollToTop()}>Start</ScrollLink>
                     </li>
                     <li className="nav__list-el">
-                        <ScrollLink activeClass="active" to="/simpleSteps" className="menu__list-link" spy={true} smooth={true}>O co chodzi?</ScrollLink>
+                        <ScrollLink activeClass="active" to="simpleSteps" className="menu__list-link" spy={true} smooth={true} duration={1000}>O co chodzi?</ScrollLink>
                     </li>
                     <li className="nav__list-el">
-                        <ScrollLink activeClass="active" to="/about" className="menu__list-link" spy={true} smooth={true}>O nas</ScrollLink>
+                        <ScrollLink activeClass="active" to="about" className="menu__list-link" spy={true} smooth={true} duration={1000}>O nas</ScrollLink>
                     </li>
                     <li className="nav__list-el">
-                        <ScrollLink activeClass="active" to="/organizations" className="menu__list-link" spy={true} smooth={true}>Fundacja i organizacje</ScrollLink>
+                        <ScrollLink activeClass="active" to="organizations" className="menu__list-link" spy={true} smooth={true} duration={1000}>Fundacja i organizacje</ScrollLink>
                     </li>
                     <li className="nav__list-el">
-                        <ScrollLink activeClass="active" to="/contact" className="menu__list-link" spy={true} smooth={true}>Kontakt</ScrollLink>
+                        <ScrollLink activeClass="active" to="contact" className="menu__list-link" spy={true} smooth={true} duration={1000}>Kontakt</ScrollLink>
                     </li>
                 </ul>
             </nav>
