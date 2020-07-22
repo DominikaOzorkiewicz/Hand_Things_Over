6ba2a6f60d67a5a4d8f65fbedc2c41754fbfc450
DominikaOzorkiewicz <62950674+DominikaOzorkiewicz@users.noreply.github.com>
2020-07-22 18:37:09 +0200

    Add database, basic structure for Organizations component with no pagination logic

--- src/components/HomeOrganizations.js
+++ src/components/HomeOrganizations.js
@@ -1,11 +1,31 @@
 import React from "react";
+import decoration from "../assets/decoration.svg";
 
 export const HomeOrganizations = () => {
 
     return (
         <section className='organizations' id='organizations'>
 
-            <div>Komu pomagamy?</div>
+            <div className='row'>
+                <div className='col-12 organizations__title'>
+                    <h2>Komu pomagamy?</h2>
+                </div>
+                <img className='organizations__deco' src={decoration} alt='Decoration'/>
+            </div>
+
+            <div className='organizations__buttons row'>
+                <button className='organizations__buttons-single col-2'>Fundacjom</button>
+                <button className='organizations__buttons-single col-2'>Organizacjom<br/>pozarządowym</button>
+                <button className='organizations__buttons-single col-2'>Lokalnym zbiórkom</button>
+            </div>
+
+            <div className='row'>
+                <p className='organizations__text'>W naszej bazie znajdziesz listę zweryfikowanych Fundacji, z którymi współpracujemy. Możesz sprawdzić czym się zajmują, komu pomagają i czego potrzebują.</p>
+            </div>
+
+            <div className='row'>
+                Lista organizacji
+            </div>
 
         </section>
     );
