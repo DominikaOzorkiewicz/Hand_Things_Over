e98cedb044f28c1789f00c0ae4bc509592fe35fe
DominikaOzorkiewicz <62950674+DominikaOzorkiewicz@users.noreply.github.com>
2020-07-28 21:45:25 +0200

    Log in validation

--- src/components/Login.js
+++ src/components/Login.js
@@ -1,11 +1,45 @@
-import React from "react";
+import React, {useState} from "react";
 import {Header} from "./Header";
-import {Link} from "react-router-dom";
+import {Link, useHistory} from "react-router-dom";
 import decoration from "../assets/decoration.svg";
 
 export const Login = () => {
+    const [user, setUser] = useState({
+        email: '',
+        password: '',
+    });
+    const [emailError, setEmailError] = useState('');
+    const [passwordError, setPasswordError] = useState('');
+    const history = useHistory();
 
-    const handleLogIn = () => {}
+    const handleChangeData = (event) => {
+        const {name, value} = event.target;
+        setUser(prevState => ({
+            ...prevState, [name]: value
+        }));
+    }
+
+    const handleLogIn = (event) => {
+        event.preventDefault();
+        let passed = true;
+
+        if (!user.email.match(/^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/)
+            || user.email.length < 2) {
+            setEmailError('Podany email jest nieprawidłowy');
+            passed = false;
+        } else {setEmailError('')}
+
+        if (user.password.length < 6) {
+            setPasswordError('Podane hasło jest za krótkie');
+            passed = false;
+        } else {setPasswordError('')}
+
+        if (passed === true) {
+            history.push('/');
+            console.log(user);
+            console.log('Użytkownik został zalogowany');
+        }
+    }
 
     return (
         <div className='login'>
@@ -22,23 +56,45 @@ export const Login = () => {
                     <div className='login__title'>Zaloguj</div>
                     <img className='login__deco' src={decoration} alt='Dekoracja' />
 
-                    <form className='login__form'>
+                    <form className='login__form' onSubmit={handleLogIn}>
                         <div className='login__form-inputs'>
                             <label className='login__form-label'>
                                 Email
                                 <input
                                     className='login__form-input'
                                     type='email'
-                                    required
+                                    name='email'
+                                    value={user.email}
+                                    onChange={handleChangeData}
                                 />
+                                { emailError.length > 0 ?
+                                    <p className='error'>
+                                        {emailError}
+                                    </p>
+                                    :
+                                    <p className='error' hidden={true}>
+                                        {emailError}
+                                    </p>
+                                }
                             </label>
                             <label className='login__form-label'>
                                 Hasło
                                 <input
                                     className='login__form-input'
                                     type='password'
-                                    required
+                                    name='password'
+                                    value={user.password}
+                                    onChange={handleChangeData}
                                 />
+                                { passwordError.length > 0 ?
+                                    <p className='error'>
+                                        {passwordError}
+                                    </p>
+                                    :
+                                    <p className='error' hidden={true}>
+                                        {passwordError}
+                                    </p>
+                                }
                             </label>
                         </div>
 
@@ -46,7 +102,7 @@ export const Login = () => {
                             <Link to='/register' className='header__user-link'>
                                 Załóż konto
                             </Link>
-                            <button className='header__user-link login__button' onSubmit={handleLogIn}>
+                            <button className='header__user-link login__button' type='submit'>
                                 Zaloguj
                             </button>
                         </div>
