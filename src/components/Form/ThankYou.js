98be5ddf75627f7437b6c7edf4b68bc2f9df2fa8
DominikaOzorkiewicz <62950674+DominikaOzorkiewicz@users.noreply.github.com>
2020-07-31 13:53:27 +0200

    Feat ThankYou section

---
+++ src/components/Form/ThankYou.js
@@ -0,0 +1,20 @@
+import React from "react";
+import decoration from "../../assets/decoration.svg"
+
+export const ThankYou = () => {
+
+    return (
+        <section className='steps row' >
+
+            <div className='col-7 thankYou'>
+                <h3 className='thankYou__title'>
+                    Dziękujemy za przesłanie formularza.<br/>
+                    Na maila prześlemy wszelkie<br/>
+                    informacje o odbiorze.
+                </h3>
+                <img className='thankYou__deco' src={decoration} alt='Decoration' />
+            </div>
+
+        </section>
+    );
+}
