46bbfcab0fe239d28d6e8062c59d657f94ef6ca2
DominikaOzorkiewicz <62950674+DominikaOzorkiewicz@users.noreply.github.com>
2020-09-24 18:16:41 +0200

    Updates

---
+++ precache-manifest.6b8dfbc47a4fdc3054bf84af7bafdb3a.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4fa798c494a584083857a87880fd4dc",
+    "url": "/Hand_Things_Over/index.html"
+  },
+  {
+    "revision": "bf5dbd127d5b9f497877",
+    "url": "/Hand_Things_Over/static/css/main.1b90d06a.chunk.css"
+  },
+  {
+    "revision": "3feca0053791edfdd281",
+    "url": "/Hand_Things_Over/static/js/2.3021ccb9.chunk.js"
+  },
+  {
+    "revision": "d0b13a04dd0dc4c851ad16fad1cc6f51",
+    "url": "/Hand_Things_Over/static/js/2.3021ccb9.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "bf5dbd127d5b9f497877",
+    "url": "/Hand_Things_Over/static/js/main.fcf92171.chunk.js"
+  },
+  {
+    "revision": "aa9a15b1dee4d7a2eab7",
+    "url": "/Hand_Things_Over/static/js/runtime-main.d86eda2e.js"
+  },
+  {
+    "revision": "c668ffe00f4b505e9122f2318a83ef3a",
+    "url": "/Hand_Things_Over/static/media/clothes-donation.c668ffe0.svg"
+  },
+  {
+    "revision": "f4db7ddd6531361fb3777e2b8fe9e8d9",
+    "url": "/Hand_Things_Over/static/media/contact-background.f4db7ddd.jpg"
+  },
+  {
+    "revision": "2290784ed27eb2dc06f78414add6bec8",
+    "url": "/Hand_Things_Over/static/media/decoration.2290784e.svg"
+  },
+  {
+    "revision": "df65b6c0ffa42176de9927a6a55fb508",
+    "url": "/Hand_Things_Over/static/media/facebook.df65b6c0.svg"
+  },
+  {
+    "revision": "3bee335ab47afd8b714411f4841f0185",
+    "url": "/Hand_Things_Over/static/media/form-hero-img.3bee335a.jpg"
+  },
+  {
+    "revision": "e99903ed42896ab89edf6c2e1d2add9e",
+    "url": "/Hand_Things_Over/static/media/hero-img.e99903ed.jpg"
+  },
+  {
+    "revision": "91147906e8dba33519e1c0ff12d308d4",
+    "url": "/Hand_Things_Over/static/media/icon-1.91147906.svg"
+  },
+  {
+    "revision": "db91e33e563df406664bb2a87d9aaa04",
+    "url": "/Hand_Things_Over/static/media/icon-2.db91e33e.svg"
+  },
+  {
+    "revision": "2cfe9dfeec6b7c1ffba1289046649228",
+    "url": "/Hand_Things_Over/static/media/icon-3.2cfe9dfe.svg"
+  },
+  {
+    "revision": "436df0aa3f9a7dcda9f88d45bf1fe1a7",
+    "url": "/Hand_Things_Over/static/media/icon-4.436df0aa.svg"
+  },
+  {
+    "revision": "c470ec0c56e643f8ee1898ea962e753a",
+    "url": "/Hand_Things_Over/static/media/icon-arrow-down.c470ec0c.svg"
+  },
+  {
+    "revision": "9b7f6ef4fe770eee646ea281f111b15d",
+    "url": "/Hand_Things_Over/static/media/icon-arrow-up.9b7f6ef4.svg"
+  },
+  {
+    "revision": "703479fdb58a909c7d696f41dc955649",
+    "url": "/Hand_Things_Over/static/media/instagram.703479fd.svg"
+  },
+  {
+    "revision": "6fa3665144c1acf34c84e4d6a9e6b6ed",
+    "url": "/Hand_Things_Over/static/media/people.6fa36651.jpg"
+  },
+  {
+    "revision": "e54cfcfc6ba3bedf321fc036d8ba90e0",
+    "url": "/Hand_Things_Over/static/media/signature.e54cfcfc.svg"
+  },
+  {
+    "revision": "aab5aa4cf97893341cd5aed84c736cd5",
+    "url": "/Hand_Things_Over/static/media/steps-background.aab5aa4c.jpg"
+  },
+  {
+    "revision": "99f2e620a9f4302f9861282162231969",
+    "url": "/Hand_Things_Over/static/media/threecolumns-background.99f2e620.png"
+  },
+  {
+    "revision": "0631e90f7c090b0bcb8f144185a7a9ed",
+    "url": "/Hand_Things_Over/static/media/truck.0631e90f.svg"
+  }
+]);
